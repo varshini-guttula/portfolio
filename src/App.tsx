4c9d659d17ace54902f4f9df28b2f12734a4b549
varshini-guttula <varshiniguttula618@gmail.com>
2026-02-10 21:39:59 +0530

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -14,7 +14,7 @@ const App = () => (
     <TooltipProvider>
       <Toaster />
       <Sonner />
-      <BrowserRouter>
+      <BrowserRouter basename="/portfolio/">
         <Routes>
           <Route path="/" element={<Index />} />
           <Route path="/About" element={<About />} />
